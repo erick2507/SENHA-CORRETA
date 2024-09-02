b96dc308af3054e995b03a4d5e8dca44f883ff68
erick2507 <169068468+erick2507@users.noreply.github.com>
2024-09-02 12:21:20 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+ Scanner sc = new Scanner(System.in);
+
+
+System.out.println(" ola digite sua senha ");
+
+    int senha = 2000;
+    int senha1 = sc.nextInt();
+
+  System.out.println(" acesso negado ");
+
+int senha3 = 2007;
+int senha4 = sc.nextInt();
+ System.out.println(" acesso negado");
+
+
+
+
+
+
+    
+
+
+
+int SEnha = sc.nextInt();
+int NUmero = 2022;
+
+  System.out.println(" acesso permitido ");
+
+
+
+
+
+
+    
+
+    
+  sc.close();
+
+
+
+    
+  }
+
+}
